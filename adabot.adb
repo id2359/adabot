0eebeb46d6b3ed83759ce30de9d42a3cdc01b70b
Crat <id2359@yahoo.com>
2016-01-12 22:29:30 +0800

    shutdown

--- adabot.adb
+++ adabot.adb
@@ -1,9 +1,21 @@
+with Ada.Real_Time; use Ada.Real_Time;
+with Nxt.Avr;
+with NXT.Display; use NXT.Display;
+
 with World;
 with Actions;
 with Sounds;
 
+-- need to add "last chance handler" 
+-- http://www.adacore.com/adaanswers/gems/gem-112-lego-mindstorms-ada-environment-part-1/
+
 procedure Shutdown is
-   
+begin
+   Put_Line("shutting down..");
+   loop
+      delay until Clock + Seconds(1);
+      Nxt.Avr.Power_Down;
+   end loop;
 end Shutdown;
 
 procedure AdaBot is
